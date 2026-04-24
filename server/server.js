c773a25c3bcae00341df00077bb1af445e2b5310
adnarn <adnusy2023@gmail.com>
2026-04-24 08:18:06 +0100

    index.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:3000', 'https://skills-connect-flhj.vercel.app'],
   credentials: true
 }));
 
